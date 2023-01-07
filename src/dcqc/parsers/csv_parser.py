a9b1cf18811255e202bcbfd1d31dfbf5c03cdb0e
Bruno Grande <bruno.grande@sagebase.org>
2023-01-06 20:56:21 -0800

    Parse non-absolute URLs as relative to CSV folder

--- src/dcqc/parsers/csv_parser.py
+++ src/dcqc/parsers/csv_parser.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from pathlib import Path
 
 from dcqc.file import File
@@ -17,8 +18,19 @@ class CsvParser:
             for index, row in enumerate(reader, start=1):
                 yield index, row
 
+    def _row_to_file(self, row: dict[str, str]) -> File:
+        csv_path = self.path
+        url = row.pop("url")
+        if File.LOCAL_REGEX.match(url):
+            scheme, separator, resource = url.rpartition("://")
+            path = Path(resource)
+            if not path.is_absolute():
+                resource = os.path.relpath(csv_path.parent / resource)
+            url = "".join([scheme, separator, resource])
+        file = File(url, row)
+        return file
+
     def iter_targets(self):
         for _, row in self.iterrows():
-            url = row.pop("url")
-            file = File(url, row)
+            file = self._row_to_file(row)
             yield Target(file)
