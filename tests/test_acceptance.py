c0c64927ed51a631bde0de3fbaea02c1dfc7b5cc
Bruno Grande <bruno.grande@sagebase.org>
2023-01-10 07:39:44 -0800

    Switch to Synapse for the JSON report

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -5,10 +5,11 @@ import pytest
 from dcqc.parsers import CsvParser
 from dcqc.reports import JsonReport
 from dcqc.tests.test_abc import TestABC
+from dcqc.utils import open_parent_fs
 
 
 @pytest.mark.integration
-def test_json_report_generation(get_data, get_output):
+def test_json_report_generation(get_data):
     # GIVEN a list of external tests to skip (to remain self-contained)
     all_tests = TestABC.list_tests()
     skipped_tests = [test.__name__ for test in all_tests if test.is_external_test]
@@ -19,9 +20,8 @@ def test_json_report_generation(get_data, get_output):
     # AND a CSV file of TXT and TIFF files
     csv_path = get_data("files.csv")
 
-    # AND a destination for the JSON report
-    report_path = get_output("report.json")
-    report_url = report_path.as_posix()
+    # AND a remote destination for the JSON report
+    report_url = "syn://syn50696607/report.json"
 
     # WHEN the CSV file is parsed to generate the relevant QC suites
     parser = CsvParser(csv_path)
@@ -32,9 +32,10 @@ def test_json_report_generation(get_data, get_output):
     report.save(suites)
 
     # THEN the file exists
-    assert report_path.exists()
+    fs, basename = open_parent_fs(report_url)
+    assert fs.exists(basename)
 
     # AND the file can be loaded by the `json` module
-    # TODO: Replace with step to recreate the suites from the JSON report
-    with report_path.open() as infile:
-        json.load(infile)
+    with fs.open(basename) as infile:
+        contents = json.load(infile)
+    assert contents
