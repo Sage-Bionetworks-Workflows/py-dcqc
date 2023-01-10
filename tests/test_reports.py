1992feedc1cb19cbb5158d68f93e2078e61ede19
Bruno Grande <bruno.grande@sagebase.org>
2023-01-10 08:13:27 -0800

    Close code coverage gap

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -4,6 +4,13 @@ from dcqc.reports import JsonReport
 
 
 def test_for_error_when_creating_report_if_file_already_exists(get_data):
-    existing_path = get_data("test.txt").as_posix()
+    existing_url = get_data("test.txt").as_posix()
     with pytest.raises(FileExistsError):
-        JsonReport(existing_path)
+        JsonReport(existing_url)
+
+
+def test_that_a_single_object_can_be_reported_on(test_files):
+    test_file = test_files["good"]
+    report_url = "mem://report.json"
+    report = JsonReport(report_url)
+    report.save(test_file)
