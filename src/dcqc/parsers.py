b38992e6dcc6c94b3efaa9477575caef77bce7e7
Bruno Grande <bruno.grande@sagebase.org>
2023-01-13 15:59:10 -0800

    Use latest method name

--- src/dcqc/parsers.py
+++ src/dcqc/parsers.py
@@ -45,6 +45,6 @@ class CsvParser:
             # which is always the case when parsing CSV files (for now)
             first_file = target.files[0]
             file_type = first_file.get_file_type()
-            suite_cls = SuiteABC.get_suite_by_file_type(file_type)
+            suite_cls = SuiteABC.get_subclass_by_file_type(file_type)
             suite = suite_cls(target, required_tests, skipped_tests)
             yield suite
