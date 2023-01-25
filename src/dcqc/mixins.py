64c824aac8362aff88635c363eb385a8daeddb6a
Bruno Grande <bruno.grande@sagebase.org>
2023-01-24 16:11:29 -0800

    Comment out `from_dict()` abstract method for now

--- src/dcqc/mixins.py
+++ src/dcqc/mixins.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
 from dataclasses import asdict
 from pathlib import PurePath
 from typing import Any
@@ -8,7 +7,8 @@ from typing import Any
 SerializedObject = dict[str, Any]
 
 
-class SerializableMixin(ABC):
+# class SerializableMixin(ABC):
+class SerializableMixin:
     @classmethod
     def from_dict_prepare(cls, dictionary: SerializedObject) -> SerializedObject:
         """Validate and prepare dictionary for deserialization."""
@@ -51,14 +51,14 @@ class SerializableMixin(ABC):
         """
         return asdict(self, dict_factory=self.dict_factory)
 
-    @classmethod
-    @abstractmethod
-    def from_dict(cls, dictionary: SerializedObject) -> SerializableMixin:
-        """Deserialize a dictionary into a SerializableMixin object.
+    # @classmethod
+    # @abstractmethod
+    # def from_dict(cls, dictionary: SerializedObject) -> SerializableMixin:
+    #     """Deserialize a dictionary into a SerializableMixin object.
 
-        Args:
-            dictionary: A serialized object.
+    #     Args:
+    #         dictionary: A serialized object.
 
-        Returns:
-            The reconstructed object.
-        """
+    #     Returns:
+    #         The reconstructed object.
+    #     """
