f3d3556c72489c1af348d43e05c8aa442bbfbc4c
Bruno Grande <bruno.grande@sagebase.org>
2023-01-29 12:48:53 -0800

    Fix docker build script

--- src/docker/build.sh
+++ src/docker/build.sh
@@ -4,4 +4,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 pipx run --spec 'tox~=3.0' tox -e clean,build
 
-docker build -t dcqc -f "${SCRIPT_DIR}/Dockerfile" "${SCRIPT_DIR}/../.."
+TARBALL_PATH=$(ls dist/*.tar.gz)
+export TARBALL_PATH
+
+docker build \
+    -t dcqc \
+    -f "${SCRIPT_DIR}/Dockerfile" \
+    --build-arg TARBALL_PATH \
+    "${SCRIPT_DIR}/../.."
