a43ad7c87928d5d2afb128882cdd46d131878745
thomasyu888 <thomas.yu@sagebase.org>
2023-04-13 15:03:55 -0700

    lint

--- src/dcqc/tests/tests.py
+++ src/dcqc/tests/tests.py
@@ -145,13 +145,12 @@ class GrepDateTest(ExternalTestMixin, TestABC):
         file = self.get_file()
         path = file.local_path.as_posix()
         command_args = [
-            "!"            # negate exit status
-            "grep",
-            "-E",          # extended regular expression
-            "-i",          # case insensitive
-            "-a",          # treat input as text
-            "-q",          # suppress output
-            "'date|time'", # match date or time
+            "!" "grep",  # negate exit status
+            "-E",  # extended regular expression
+            "-i",  # case insensitive
+            "-a",  # treat input as text
+            "-q",  # suppress output
+            "'date|time'",  # match date or time
             path,
         ]
         process = Process(
