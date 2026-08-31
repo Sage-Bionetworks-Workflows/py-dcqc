caacef71d0187ad7a84f49b1b3717f7a3cc42962
andrewelamb <andrewelamb@gmail.com>
2026-08-31 07:20:00 -0700

    fix bug where PairedTarget could not be deserialized

--- tests/test_target.py
+++ tests/test_target.py
@@ -34,6 +34,16 @@ def test_that_paired_file_target_can_be_created_with_two_files(test_files):
     assert len(target.files) == 2
 
 
+def test_that_a_paired_target_can_be_saved_and_restored_without_changing(test_files):
+    test_file = test_files["good_txt"]
+    target_1 = PairedTarget([test_file, test_file])
+    target_1_dict = target_1.to_dict()
+    target_2 = PairedTarget.from_dict(target_1_dict)
+    target_2_dict = target_2.to_dict()
+    assert target_1 == target_2
+    assert target_1_dict == target_2_dict
+
+
 def test_for_an_error_when_creating_paired_file_target_with_one_file(test_files):
     test_file = test_files["good_txt"]
     with pytest.raises(ValueError):
