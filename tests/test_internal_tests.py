aa9e24d4963f22704d6c3008e4eee01cf6d093b5
andrewelamb <andrewelamb@gmail.com>
2026-09-25 07:24:09 -0700

    added exception match

--- tests/test_internal_tests.py
+++ tests/test_internal_tests.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 from dcqc import tests
@@ -18,11 +20,12 @@ def test_for_an_error_when_retrieving_a_test_that_does_not_exist_by_name():
 def test_for_error_when_importing_unavailable_module(test_targets):
     target = test_targets["good_txt"]
     test = tests.FileExtensionTest(target)
-    with pytest.raises(ModuleNotFoundError) as excinfo:
+    with pytest.raises(
+        ModuleNotFoundError, match=re.escape("pip install dcqc[all]")
+    ) as excinfo:
         test.import_module("foobar")
     # The message must be a single string, not a tuple of string fragments.
     assert isinstance(excinfo.value.args[0], str)
-    assert "pip install dcqc[all]" in excinfo.value.args[0]
 
 
 def test_that_an_existing_module_can_be_imported(test_targets):
